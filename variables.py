9fa4afc80852a3440554e5e72d0eabdde0387350
JRudransh <bhaskaruprety3@gmail.com>
2020-01-29 13:43:19 +0530

    Set variables

---
+++ variables.py
@@ -0,0 +1,11 @@
+from time import time
+# Path for project file
+local_path = "/Documents/Python_Advance_Concept/Clipboard_History"
+git_path = "https://github.com/JRudransh/Clipboard_History"
+
+# Location variables
+folder = "clipboard_data"
+error_folder = ".error_log"
+dynamic_filename = str(time()).replace(".", "") + ".txt"
+limit = 100
+
